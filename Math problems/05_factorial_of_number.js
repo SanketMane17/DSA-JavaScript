c24f74b4eabc6783e07688fa65a96dcc3b8243c8
Sanket <msanket450@gmail.com>
2022-07-26 15:15:54 +0530

    Factorial using recursion

--- Math problems/05_factorial_of_number.js
+++ Math problems/05_factorial_of_number.js
@@ -1,14 +1,21 @@
-let fact = 1;
+// function factorial(num) {
+//     let fact = 1;
+//     if(num == 0) {
+//         return 1;
+//     }
+
+//     for(let i = num;i > 0;i--) {
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+
+// By using recursion
 
 function factorial(num) {
-    if(num == 0) {
+    if(num == 0 || num == 1) 
         return 1;
-    }
-
-    for(let i = num;i > 0;i--) {
-        fact = fact * i;
-    }
-    return fact;
+    return num * factorial(num - 1);
 }
 
 console.log(factorial(5));
